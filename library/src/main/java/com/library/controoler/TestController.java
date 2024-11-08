8f7fd96801268648d5872e5d5eeed766be6b5476
Gohar-Elsaqq <gohar.elsaqqa@gmail.com>
2024-11-08 07:28:50 +0200

    Test updated with contact information

--- library/src/main/java/com/library/controoler/TestController.java
+++ library/src/main/java/com/library/controoler/TestController.java
@@ -4,11 +4,19 @@ package com.library.controoler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestController
 public class TestController {
 
     @GetMapping("/test")
-    public String testEndpoint() {
-        return "Application is running gohar!";
+    public Map<String, String> testEndpoint() {
+        Map<String, String> response = new HashMap<>();
+        response.put("email", "gohar.elsaqqa@gmail.com");
+        response.put("emailLink", "mailto:gohar.elsaqqa@gmail.com");
+        response.put("phone", "01060415152");
+        response.put("whatsappLink", "https://wa.me/201060415152");
+        return response;
     }
 }
