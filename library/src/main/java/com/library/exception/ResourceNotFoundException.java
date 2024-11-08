3a980434cd21d499bd35c32a828feb251efa4157
Gohar-Elsaqq <gohar.elsaqqa@gmail.com>
2024-11-08 07:50:36 +0200

    Enhance ResourceNotFoundException with detailed error messages and improved handling in GlobalExceptionHandler

--- library/src/main/java/com/library/exception/ResourceNotFoundException.java
+++ library/src/main/java/com/library/exception/ResourceNotFoundException.java
@@ -3,6 +3,7 @@ package com.library.exception;
 public class ResourceNotFoundException extends RuntimeException {
     public ResourceNotFoundException() {
         super();
+
     }
 
     public ResourceNotFoundException(String message) {
