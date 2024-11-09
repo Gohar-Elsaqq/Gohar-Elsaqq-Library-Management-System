ad9468a45b74a50e436b35f4993a50f46fd2194c
Gohar-Elsaqq <gohar.elsaqqa@gmail.com>
2024-11-09 11:21:44 +0200

    update test

--- library/src/test/java/com/library/LibraryApplicationTests.java
+++ library/src/test/java/com/library/LibraryApplicationTests.java
@@ -1,11 +1,13 @@
 package com.library;
 
 import com.library.dto.BookDTO;
+import com.library.repository.BookRepository;
 import com.library.service.BookService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.time.LocalDate;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -16,11 +18,14 @@ class LibraryApplicationTests {
 	@Autowired
 	private BookService bookService;
 
-//	@Test
-//	void findByIdNotFound() {
-//		 Optional<BookDTO> book=bookService.getBookById(99L);
-//		assertEquals("Book with ID 99 not found.",false);
-//	}
+	@Autowired
+	private BookRepository bookRepository;
+
+	@Test
+	void findByIdNotFound() {
+		 Optional<BookDTO> book=bookService.getBookById(99L);
+		assertEquals("Book with ID 99 not found.",false);
+	}
 
 	@Test
 	void findById() {
@@ -34,4 +39,24 @@ class LibraryApplicationTests {
 		System.out.println("return book  ");
 	}
 
+
+
+	@Test
+	public void testSaveBook() {
+		BookDTO bookDTO = new BookDTO();
+		bookDTO.setTitle("Clean Code");
+		bookDTO.setAuthor("Robert C. Martin");
+		bookDTO.setIsbn("978-0132350884");
+		bookDTO.setPublicationYear(2003);
+
+		BookDTO savedBook = bookService.saveBook(bookDTO);
+
+		assertNotNull(savedBook.getId());
+		assertEquals("Clean Code", savedBook.getTitle());
+		assertEquals("Robert C. Martin", savedBook.getAuthor());
+		assertEquals("978-0132350884", savedBook.getIsbn());
+		System.out.println("save book ");
+		bookRepository.deleteById(savedBook.getId());
+		System.out.println(savedBook);
+	}
 }
